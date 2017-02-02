7a3429c00847eb5fdf23233707c4ee9ce92b9e14
Ray-E-Artz <ray.e.artz@gmail.com>
2017-02-01 19:40:06 -0600

    After page 50; not complete

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,24 @@
+/**
+ * Created by Ray on 2/1/17.
+ */
+
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number){
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+}
